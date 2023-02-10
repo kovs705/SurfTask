528f950eb2f660ee5f8f0a42c469236472f5059c
kovs705 <smartkovsik@gmail.com>
2023-02-10 19:14:34 +0300

    - Changed image into sticky header - Configured the formView - Preparing for UIScrollView pagination and adding button + bottomBar!!! So far so good)))

--- SurfTask/Extensions/ViewControllerExt.swift
+++ SurfTask/Extensions/ViewControllerExt.swift
@@ -9,19 +9,111 @@ import UIKit
 
 class VCExt: UIViewController {
     
+    // MARK: - Properties
+    
+    let imageContainer: UIView = {
+        let imageContainer = UIView()
+        imageContainer.backgroundColor = .clear
+        imageContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageContainer
+    }()
+    
+    let backImage: UIImageView = {
+        let back = UIImageView()
+        back.backgroundColor = .black
+        back.image = UIImage(named: "iceland")
+        back.contentMode = .scaleAspectFill
+        back.clipsToBounds = true
+        
+        back.translatesAutoresizingMaskIntoConstraints = false
+        
+        return back
+    }()
+    
+    let formView: UIView = {
+        let formView = UIView()
+        formView.backgroundColor = .white
+        formView.layer.cornerRadius = 40
+        
+        formView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return formView
+    }()
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.alwaysBounceVertical = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.backgroundColor = .systemBackground
+        scrollView.alwaysBounceHorizontal = false
+        scrollView.contentInsetAdjustmentBehavior = .never
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
     
     
+    // MARK: - Functions
+    
+    func configureFormView() {
+        let g = scrollView.contentLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            formView.widthAnchor.constraint(equalToConstant: view.bounds.width),
+            formView.heightAnchor.constraint(equalToConstant: view.bounds.height - 80),
+            
+            formView.topAnchor.constraint(equalTo: imageContainer.bottomAnchor, constant: -40),
+            formView.leadingAnchor.constraint(equalTo: g.leadingAnchor),
+            formView.trailingAnchor.constraint(equalTo: g.trailingAnchor),
+            formView.bottomAnchor.constraint(equalTo: g.bottomAnchor)
+        ])
+    }
     
     
+    func configureContainer() {
+        let g = scrollView.contentLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            imageContainer.widthAnchor.constraint(equalToConstant: view.bounds.width),
+            imageContainer.heightAnchor.constraint(equalToConstant: view.bounds.height * 0.65),
+            
+            imageContainer.topAnchor.constraint(equalTo: g.topAnchor, constant: -50),
+            imageContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
     
     
+    func configureImage() {
+        let topConstant = backImage.topAnchor.constraint(equalTo: view.topAnchor, constant: -50)
+        topConstant.priority = .defaultHigh
+
+        let heightConstraint = backImage.heightAnchor.constraint(greaterThanOrEqualTo: imageContainer.heightAnchor)
+        heightConstraint.priority = .required
+        
+        NSLayoutConstraint.activate([
+            backImage.widthAnchor.constraint(equalToConstant: view.bounds.width),
+            heightConstraint,
+            
+            topConstant,
+            backImage.leadingAnchor.constraint(equalTo: imageContainer.leadingAnchor),
+            backImage.bottomAnchor.constraint(equalTo: imageContainer.bottomAnchor)
+
+        ])
+    }
     
     
-    
-    
-    
-    
-    
-    
+    func configureSV() {
+        NSLayoutConstraint.activate([
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+    }
+
     
 }
--- SurfTask/VCs/ViewController.swift
+++ SurfTask/VCs/ViewController.swift
@@ -7,39 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    let backImage: UIImageView = {
-        let back = UIImageView()
-        back.backgroundColor = .black
-        back.image = UIImage(named: "iceland")
-        
-        back.translatesAutoresizingMaskIntoConstraints = false
-        
-        return back
-    }()
-    
-    let formView: UIView = {
-        let formView = UIView()
-        formView.backgroundColor = .green
-        formView.layer.cornerRadius = 20
-        
-        formView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return formView
-    }()
-    
-    let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.alwaysBounceVertical = true
-        scrollView.backgroundColor = .systemBackground
-        scrollView.alwaysBounceHorizontal = false
-        
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return scrollView
-    }()
-    
+class ViewController: VCExt {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,36 +15,19 @@ class ViewController: UIViewController {
         self.view.addSubview(scrollView)
         configureSV()
         
+        scrollView.addSubview(imageContainer)
+        configureContainer()
+        
         scrollView.addSubview(backImage)
         configureImage()
-
-    }
-    
-    func configureImage() {
-        let g = scrollView.contentLayoutGuide
         
-        NSLayoutConstraint.activate([
-            backImage.widthAnchor.constraint(equalToConstant: view.bounds.width),
-            backImage.heightAnchor.constraint(equalToConstant: view.bounds.height * 0.65),
-            
-            backImage.topAnchor.constraint(equalTo: g.topAnchor, constant: 16),
-            backImage.leadingAnchor.constraint(equalTo: g.leadingAnchor),
-            backImage.trailingAnchor.constraint(equalTo: g.trailingAnchor),
-            
-            backImage.bottomAnchor.constraint(equalTo: g.bottomAnchor, constant: -(2000.0 - (100.0 + 16.0)))
-        ])
-    }
-    
-    
-    
-    func configureSV() {
-        NSLayoutConstraint.activate([
-            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
+        scrollView.addSubview(formView)
+        configureFormView()
+        
         
     }
+    
+    
+    
 }
 
